dd4fe4476190b692a69261bee99e8f7fbebf4ef1
smedbergville <smedbergs@icloud.com>
2025-12-21 15:35:20 +0100

    Create Work.cs

---
+++ Models/Work.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CvProjekt.Models
+{
+    public class Work
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Description { get; set; }
+        public int ResumeId { get; set; }
+
+        [ForeignKey(nameof(ResumeId))]
+        public virtual Resume Resume { get; set; }
+    }
+}
