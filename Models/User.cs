ecf3192366fc4e55d3df4d9217cd0fa9f99100c7
03Nea <linneag03@gmail.com>
2025-12-21 12:29:59 +0100

    påbörjat user

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CvProjekt.Models
+{
+    public class User{
+        public int Id {get; set; }
+
+        public string Name {get; set;}
+
+        public string Adress {get; set;}
+
+        public string Email {get; set;}
+
+        
+    }
+}
