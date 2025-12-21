1e9cb2c603335818d521a02ff403691e3fe82c0f
smedbergville <smedbergs@icloud.com>
2025-12-21 15:26:38 +0100

    Create Education.cs

---
+++ Models/Education.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CvProjekt.Models
+{
+    public class Education
+    {
+        public int Id { get; set; }
+        public string SchoolName { get; set; }
+        public string DegreeName { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public string Description { get; set; }
+        public int ResumeId { get; set; }
+
+        [ForeignKey(nameof(ResumeId))]
+        public virtual Resume Resume { get; set; }
+    }
+}
