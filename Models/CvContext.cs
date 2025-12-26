f2b8f51b99009d9ec4260006c5b0db33545906da
03Nea <linneag03@gmail.com>
2025-12-26 15:04:21 +0100

    fix igen

--- Models/CvContext.cs
+++ Models/CvContext.cs
@@ -10,7 +10,6 @@ namespace CvProjekt.Models
         {
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Resume> Resumes { get; set; }
         public DbSet<Project> Projects { get; set; }
 
