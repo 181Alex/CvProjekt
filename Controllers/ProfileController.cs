e82365aff36e823943fde5b22341a639ef238e29
03Nea <linneag03@gmail.com>
2025-12-27 14:53:38 +0100

    fix av profilsida

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -23,7 +23,31 @@ namespace CvProjekt.Controllers
 
         public async Task<IActionResult> MyProfile()
         {
-            Console.WriteLine("Nu körs MyProfile action!");
+            
+            var userId = "user-1";
+               
+               //_userManager.GetUserId(User);
+
+            var user = await _context.Users
+                        .Include(u => u.Projects)
+                        .Include(u => u.Resume)
+                            .ThenInclude(r => r.Qualifications)
+                        .Include(u => u.Resume)
+                            .ThenInclude(r => r.WorkList)
+                        .Include(u => u.Resume)
+                            .ThenInclude(r => r.EducationList)
+                        .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if(user == null){
+                return Content($"Fel: Hittade ingen användare med ID '{userId}' i databasen. Har du kört database update?");
+            }
+
+            return View(user);
+
+        }
+
+        public async Task<IActionResult> EditResume()
+        {
             
             var userId = "user-1";
                
