69d5443b642bf4427258f5706af65053c48f9927
181Alex <alex.2003.a@gmail.com>
2025-12-27 21:05:30 +0100

    olästa meddelanden i headerna

---
+++ ViewComponents/UnreadMessagesViewComponent.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CvProjekt.Models;
+
+
+
+namespace CvProjekt.ViewComponents
+{
+    public class UnreadMessagesViewComponent :ViewComponent{
+
+        private readonly CvContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public UnreadMessagesViewComponent(CvContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            // Om ingen är inloggad, visa 0
+            if (user == null) return View(0);
+
+            // Räkna olästa meddelanden till mig
+            var count = await _context.Messages
+                .Where(m => m.ToUserId == user.Id && !m.Read)
+                .CountAsync();
+
+            return View(count);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
