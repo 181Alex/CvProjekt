232b60e520908aa7e883dc70ccf9dcbdc6e730f2
SimonE147 <122627592+SimonE147@users.noreply.github.com>
2026-01-08 12:01:23 +0100

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// CvProjekt/wwwroot/js/site.js
+
+
+function seMoreProfilesNow() {
+    var latestProfiles = document.getElementById("LatestProfilesBox");
+    var allProfiles = document.getElementById("AllProfilesBox");
+
+    if (latestProfiles && allProfiles) {
+        latestProfiles.style.display = 'none';
+        allProfiles.style.display = 'block';
+    }
+}
+
+
+function seLessProfilesNow() {
+    var latestProfiles = document.getElementById("LatestProfilesBox");
+    var allProfiles = document.getElementById("AllProfilesBox");
+
+    if (latestProfiles && allProfiles) {
+        latestProfiles.style.display = 'flex';
+        allProfiles.style.display = 'none';
+    }
+}
